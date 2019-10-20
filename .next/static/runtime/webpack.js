a284d6b15ca18e9f09010d96f7cdb262d346f253
Josué Gimenes <josuegalvares@gmail.com>
2019-10-20 12:38:53 -0300

    Axios settings to consume api github

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "61c56cde3a15f0ff829c";
+/******/ 	var hotCurrentHash = "56d57ab5d8b5be38426b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
