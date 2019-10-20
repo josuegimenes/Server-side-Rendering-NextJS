ec4ae161b24d31b2598a5b90e46a26239ff6c17a
Josué Gimenes <josuegalvares@gmail.com>
2019-10-20 16:05:16 -0300

    Create Readme

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d4567e57f31ce107caa2";
+/******/ 	var hotCurrentHash = "31533949e7075b727c38";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ README.md
@@ -0,0 +1,7 @@
+# Server-side Rendering com NextJS
+
+Desenvolvimento de uma estrutura para a busca de Membros de uma Organização no Github.
+
+### Aplicação
+
+![Members Organization GitHub](assets/example.gif)
